359e91c125bc59c83d085b3891b686dae2efd142
Kevin Xue <kevin@dhcp-10-105-126-105.wireless.northwestern.private>
2016-09-26 11:22:13 -0500

    add event 3 and 4

--- Frood/www/scripts/application.js
+++ Frood/www/scripts/application.js
@@ -23,8 +23,28 @@ angular.module('SteroidsApplication', [
           desc: "Come on Wednesdays to meet awesome people and learn one of the most beautiful dances in the world.",
           show: false
     }
+
+  $scope.testEvent3 = {
+          name: "Library GO",
+          loc: " University Library, 1970 Campus Drive   ",
+          time: "Wed, 10:00 AM - 5:00 PM  ",
+          foodType: "Pizza & T-shirt",
+          desc: "Find the Pokemon we've hidden around University Library and snap their pictures for a free T-Shirt.",
+          show: false
+    }
+
+  $scope.testEvent4 = {
+          name: "Hack Night is back",
+          loc: "The Garage",
+          time: "Wed, 7pm",
+          foodType: "Koi",
+          desc: "Know the basics but aren't too sure on how to make your beautiful website public to the world? Come to our first Hack Night of the year to get started with web development!",
+          show: false
+    }
   $scope.foodEvents.push($scope.testEvent2)
   $scope.foodEvents.push($scope.testEvent1)
+  $scope.foodEvents.push($scope.testEvent3)
+  $scope.foodEvents.push($scope.testEvent4)
 
 
 });
